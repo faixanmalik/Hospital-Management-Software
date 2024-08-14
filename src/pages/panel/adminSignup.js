460d40cb5090bf8e63ab2490b52cd6d631d88d51
faixanmalik <faixan.malik554@gmail.com>
2024-08-14 16:09:50 +0500

    update

--- src/pages/panel/adminSignup.js
+++ src/pages/panel/adminSignup.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from 'next/link'
 import Header from "@/components/Header";
 import { toast, ToastContainer } from "react-toastify";
