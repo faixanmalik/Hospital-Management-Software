de73dd0940d643e89a8942099c494c26e0569a89
faixanmalik <faixan.malik554@gmail.com>
2024-08-15 13:20:12 +0500

    add button in website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-   <main className='bg-baseColor'>
-    frontend- website
+   <main className='w-full'>
+
+      <Link
+        href={'/panel'}
+        className="mt-2 bg-baseColor hover:bg-hoverBaseColor flex justify-center items-center px-5 py-2 text-sm font-semibold text-cardColor border-none rounded-md"
+      >
+        Go to Panel
+      </Link>
+
+
    </main>
   )
 }
